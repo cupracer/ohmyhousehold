8c01895d15ceef5969d9043d23499f3f95a18cb8
Thomas Schulte <thomas@cupracer.de>
2023-02-08 20:16:19 +0100

    add Redis to handle session data

---
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,49 @@
+<?php
+
+// Making the Locale "Sticky" during a User's Session:
+// Symfony stores the locale setting in the Request, which means that this setting is not automatically saved ("sticky")
+// across requests. But, you can store the locale in the session, so that it's used on subsequent requests.
+//
+// Source: https://symfony.com/doc/current/session.html
+// TODO: Implement: Setting the Locale Based on the User's Preferences
+// TODO: Implement: Encryption of Session Data
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    private $defaultLocale;
+
+    public function __construct(string $defaultLocale = 'en')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        // try to see if the locale has been set as a _locale routing parameter
+        if ($locale = $request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+            // if no explicit locale has been set on this request, use one from the session
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            // must be registered before (i.e. with a higher priority than) the default Locale listener
+            KernelEvents::REQUEST => [['onKernelRequest', 20]],
+        ];
+    }
+}
