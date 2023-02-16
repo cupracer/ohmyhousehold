060f7c4a0cbb308f5b2f63cb1f271e2e07df141f
Thomas Schulte <thomas@cupracer.de>
2023-02-16 18:45:53 +0100

    add common forms.js import file used for form pages

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      */
     .addEntry('app', './assets/app.js')
     .addEntry('datatables', './assets/datatables.js')
+    .addEntry('forms', './assets/forms.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
