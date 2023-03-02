969e54b0e7f549b72f05f8ba2febf87dee754c13
Thomas Schulte <thomas@cupracer.de>
2023-03-02 10:33:52 +0100

    move JavaScript code to central barcode scanner function

--- assets/barcode-scanner.js
+++ assets/barcode-scanner.js
@@ -20,4 +20,67 @@
 import Quagga from '@ericblade/quagga2/dist/quagga';
 
 // make Quagga available globally
-window.Quagga = Quagga;
+// window.Quagga = Quagga;
+
+window.addBarcodeListenerAndUpdateSelect2field =
+
+    function addBarcodeListenerAndUpdateSelect2field(elementId, select2field) {
+        // Event listener for the image upload
+        document.getElementById(elementId).addEventListener("change", function(e) {
+            let fileInputElement = e.target;
+            // console.log("Image input changed: " + fileInputElement.id);
+
+            // get the chosen file
+            if(fileInputElement.files.length !== 1 || fileInputElement.files[0].type.indexOf("image/") !== 0) {
+                console.log("No image file selected");
+                return;
+            }
+
+            let file = fileInputElement.files[0];
+
+            // create a URL for the image
+            let imgUrl = URL.createObjectURL(file);
+
+            // create an image object
+            let img = new Image();
+            img.onload = function() {
+                // console.log("Image loaded. Processing...");
+                processImage(imgUrl);
+            };
+
+            // load the image to trigger the onload event
+            img.src = imgUrl;
+
+            // reset the input element to allow the same file to be selected again
+            fileInputElement.value = '';
+        });
+
+        // Process the provided image with Quagga
+        function processImage(img) {
+            // console.log("Image processing started...");
+            Quagga.decodeSingle({
+                decoder: { readers: ["ean_8_reader", "ean_reader"] },
+                locate: true,
+                src: img
+            }, function(result) {
+                if(result && result.codeResult) {
+                    // console.log("Barcode detected and processed : [" + result.codeResult.code + "]", result);
+                    // write the barcode to the selected element
+                    // productSelect.select2('open');
+                    select2field.data('select2').dropdown.$search.val(result.codeResult.code).trigger('input');
+
+                    // productSelect.val(result.codeResult.code).trigger('change');
+                    // console.log("Select2 value set to " + result.codeResult.code + ". Triggered change event.");
+                } else {
+                    console.log("Barcode could not be detected");
+                    $(document).Toasts('create', {
+                        class: 'bg-danger',
+                        body: "{{ 'app.supplies.article.barcode.notfound'|trans }}",
+                        autohide: true,
+                        delay: 4000,
+                        icon: 'fas fa-exclamation fa-lg',
+                    });
+                }
+            });
+        }
+    }
